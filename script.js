2a7149786ea20396bd6621de1ad0a140519b702c
Millie Ren <renyifei2017@hotmail.com>
2025-09-05 18:42:26 +0800

    set up tab-switch function make sure code is clean

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 const tabs = document.querySelectorAll('.tab');
 const panels = document.querySelectorAll('.panel');
-let choice = -1;
+let selectedIndex = -1;
 
-tabs.forEach((tab, index, arr) => {
-    tab.index = index;
+tabs.forEach((tab, index) => {
     tab.addEventListener('click', () => {
-        tabClick(tab);
+        handleTabClick(index);
     })
-})
+});
+
+function handleTabClick (index) {
+    if (index === selectedIndex) return;
+
+    // 取消选择现在的标签
+    if (selectedIndex >= 0) {
+        panels[selectedIndex].classList.remove('enable');
+    }
+
+    // 选择标签
+    let panel = panels[index];
+    panel.classList.add('enable');
+    selectedIndex = index;
+};
+
+handleTabClick(0);
